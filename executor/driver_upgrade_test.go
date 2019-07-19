c6f22161e48332e157fc27a973e8dd38e3bc544d
liupzmin <676333678@qq.com>
2019-07-19 15:27:13 +0800

    disable upgrade test

--- executor/driver_upgrade_test.go
+++ executor/driver_upgrade_test.go
@@ -1,14 +1,10 @@
 package executor
 
-import (
-	"testing"
-)
-
-func TestRollBackNormal(t *testing.T) {
+/*func TestRollBackNormal(t *testing.T) {
 	u := &Upgrade{Service: &Service{Dir: "/home/alarm/AlarmWechat", OsUser:"alarm"}, backfile: "/home/alarm/Backup/AlarmWechat20190524110559.143.zip"}
 	err := u.rollBack()
 
 	if err != nil {
 		t.Errorf(`Download("/home/alarm/AlarmWechat %s") = error`, err.Error())
 	}
-}
+}*/
