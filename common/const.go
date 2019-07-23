4b1a7d6a0b26ee2b4b2219dbfa28319959354b3b
liupzmin <676333678@qq.com>
2019-07-23 16:25:46 +0800

    change operation code

--- common/const.go
+++ common/const.go
@@ -7,20 +7,14 @@ package common
 type OpMode int32
 
 const (
-	Operate_DEP OpMode = 0 //deploy
-	Operate_UPG OpMode = 1 //upgrade
-	Operate_STA OpMode = 2 //start
-	Operate_SHU OpMode = 3 //stop
-	Operate_RES OpMode = 4 //restart
-	Operate_CHE OpMode = 5 //check
-)
-
-type FileOpMode int32
-
-const (
-	FileOp_ADD  FileOpMode = 0 //添加字段
-	FileOp_DEL  FileOpMode = 1 //删除字段
-	FileOp_EDIT FileOpMode = 2 //修改字段
+	Operate_DEP OpMode = 1 //deploy
+	Operate_UPG OpMode = 2 //upgrade
+	Operate_STA OpMode = 3 //start
+	Operate_SHU OpMode = 4 //stop
+	Operate_RES OpMode = 5 //restart
+	Operate_CHE OpMode = 6 //check
+	Operate_BAK OpMode = 7 //backup
+	Operate_ROL OpMode = 8 //rollback
 )
 
 type ExecuteReturnCode int32
@@ -29,14 +23,6 @@ const (
 	ReturnCode_FAILED    ExecuteReturnCode = 0 //执行失败
 	ReturnCode_SUCCESS   ExecuteReturnCode = 1 //执行成功
 	ReturnCode_Exception ExecuteReturnCode = 2 //执行异常
-)
-
-type ServiceExistCode int32
-
-const (
-	ServiceNonExist      ServiceExistCode = 0
-	ServiceExistAndMatch ServiceExistCode = 1
-	ServiceExistNotMatch ServiceExistCode = 2
 )
 
 const (
